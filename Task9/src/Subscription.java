76d5deb981889fd1f0d922b72b3af07f4b12da5e
hermonj-spec <hermon.samsonj@gmail.com>
2025-11-05 18:57:15 +0300

    Commit 2
    In the new version on line 12 and 20,  method that Overrides the displayDetails abstract method and an overridden discountedPrice method in Subscription class to include the fields in the output was added.

--- Task9/src/Subscription.java
+++ Task9/src/Subscription.java
@@ -7,4 +7,19 @@ public class Subscription extends LaptopStore {
         this.durationMonths = durationMonths;
         this.renewalStatus = renewalStatus;
     }
+
+    @Override
+    void displayDetails() {
+        System.out.println("Product Name: " + name);
+        System.out.println("Price: " + price);
+        System.out.println("Duration: " + durationMonths + "hours");
+        System.out.println("Provider: " + renewalStatus);
+    }
+
+    @Override
+    public void discountedPrice(double discount) {
+        double total = price * durationMonths;
+        double discountedTotal = total - (total * discount);
+        System.out.println("Discounted Annual Price: $" + discountedTotal);
+    }
 }
