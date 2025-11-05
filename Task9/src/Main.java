d0cbd677a067d2ef5d6a9ed6d880ae62fe28951a
hermonj-spec <hermon.samsonj@gmail.com>
2025-11-05 19:03:30 +0300

    Commit 1
    A new Main class with a main method that creates objects of Product, Service and Subscription was added to the repository.

---
+++ Task9/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Product product1 = new Product("HP G16", 2499.99, "Electronics", 5);
+        Service service1 = new Service("Computer Maintenance", 120.00, 2, "Big-Tech");
+        Subscription subscription1 = new Subscription("Premium Subscription", 18.99, 12, "Enabled");
+    }
+}
