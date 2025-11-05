532c5b27319f0fbb08ddbc9d9cffe1b51a81f22a
hermonj-spec <hermon.samsonj@gmail.com>
2025-11-05 19:06:39 +0300

    Commit 2
    In the new version method calling of each object created on the main method was added.

--- Task9/src/Main.java
+++ Task9/src/Main.java
@@ -3,5 +3,19 @@ public class Main {
         Product product1 = new Product("HP G16", 2499.99, "Electronics", 5);
         Service service1 = new Service("Computer Maintenance", 120.00, 2, "Big-Tech");
         Subscription subscription1 = new Subscription("Premium Subscription", 18.99, 12, "Enabled");
+
+        System.out.println("=== Product Details ===");
+        product1.displayDetails();
+        product1.discountedPrice(0.10);
+        System.out.println();
+
+        System.out.println("=== Service Details ===");
+        service1.displayDetails();
+        service1.discountedPrice(0.15);
+        System.out.println();
+
+        System.out.println("=== Subscription Details ===");
+        subscription1.displayDetails();
+        subscription1.discountedPrice(0.20);
     }
 }
