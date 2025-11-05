3dd6495b4095fea60b30a7b0cc805399a199c75c
hermonj-spec <hermon.samsonj@gmail.com>
2025-11-05 18:48:39 +0300

    Commit 2
    In the new version on line 12, a method that Overrides the displayDetails abstract method in Product class to include the fields in the output was added.

--- Task9/src/Product.java
+++ Task9/src/Product.java
@@ -7,4 +7,12 @@ public class Product extends LaptopStore {
         this.category = category;
         this.units = units;
     }
+
+    @Override
+    void displayDetails() {
+        System.out.println("Product Name: " + name);
+        System.out.println("Price: " + price);
+        System.out.println("Category: " + category);
+        System.out.println("Available units: " + units);
+    }
 }
