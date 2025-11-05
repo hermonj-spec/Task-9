2d7eff17e94c88373f13803ab1874898b81ff4a6
hermonj-spec <hermon.samsonj@gmail.com>
2025-11-05 18:44:54 +0300

    Commit 1
    A new LaptopStore abstract class containing name and price attributes, along with a constructor to initialize these fields was added to the repository.

---
+++ Task9/src/LaptopStore.java
@@ -0,0 +1,9 @@
+public abstract class LaptopStore {
+    protected String name;
+    protected double price;
+
+    public LaptopStore(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+}
